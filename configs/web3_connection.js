8471223c233fe5f93f0d0d6c68291b173fef3753
Slowqueso <lyndabarn2309@gmail.com>
2023-07-07 17:34:02 +0530

    prod config

--- configs/web3_connection.js
+++ configs/web3_connection.js
@@ -1,13 +1,24 @@
 import Web3 from "web3";
 import fs from "fs";
+
+const PROD = process.env.PRODUCTION;
+let ETHEREUM_NODE_URL, ETHERUM_CHAIN_ID, MINERVA_CONTRACT_ADDRESS;
+
+if (PROD === "true") {
+  ETHEREUM_NODE_URL = process.env.ETHEREUM_NODE_URL;
+  ETHERUM_CHAIN_ID = process.env.ETHERUM_CHAIN_ID;
+  MINERVA_CONTRACT_ADDRESS = process.env.MINERVA_CONTRACT_ADDRESS;
+} else {
+  ETHEREUM_NODE_URL = process.env.ETHEREUM_DEV_NODE_URL;
+  ETHERUM_CHAIN_ID = process.env.ETHERUM_DEV_CHAIN_ID;
+  MINERVA_CONTRACT_ADDRESS = process.env.MINERVA_DEV_CONTRACT_ADDRESS;
+}
+
 const parsedABI = JSON.parse(
   fs.readFileSync("./constants/smart_contract_details/abi.json")
 );
-const web3 = new Web3(process.env.ETHEREUM_NODE_URL);
-const contract = new web3.eth.Contract(
-  parsedABI,
-  process.env.MINERVA_CONTRACT_ADDRESS
-);
+const web3 = new Web3(ETHEREUM_NODE_URL);
+const contract = new web3.eth.Contract(parsedABI, MINERVA_CONTRACT_ADDRESS);
 const accounts = await web3.eth.getAccounts();
 
 async function connectToDevchain() {
